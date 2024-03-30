2720d598068fc4782894cde1963730002b222c11
a-stands4-A <akim.idrisov@mail.ru>
2024-03-30 10:04:53 +0300

    maze task description completed

--- main.py
+++ main.py
@@ -56,32 +56,32 @@ digraph sch21 {
         
         b [label="Что тут может происходить"];
         
-        # subgraph clusterF1 {
-        #     bgcolor=green;
-        #     pencolor=black;
-        #     label = "void smt1 (ch str, double c)";
-        #     labeljust=c;
-        #     descr_1 [shape=plaintext label=<
-        #     <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
-        #           <TR>
-        #                 <TD PORT="descr"  BGCOLOR="darkgrey">longlonglonglonglong description<BR/>description<BR/>description</TD>
-        #                 <TD PORT="return" BGCOLOR="orange">return</TD>
-        #           </TR>
-        #     </TABLE>>];        }
-        # 
-        # subgraph clusterF0 {
-        #     bgcolor=green;
-        #     pencolor=black;
-        #     label = "int smt0 (int pr01, float pr02)";
-        #     labeljust=c; 
-        #     descr_0 [shape=plaintext label=<
-        #     <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
-        #           <TR>
-        #                 <TD PORT="descr"  BGCOLOR="darkgrey">longlonglonglonglong description<BR/>description<BR/>description</TD>
-        #                 <TD PORT="return" BGCOLOR="orange">return</TD>
-        #           </TR>
-        #     </TABLE>>];
-        # }
+        subgraph clusterF1 {
+            bgcolor=green;
+            pencolor=black;
+            label = "void smt1 (ch* str, double c)";
+            labeljust=c;
+            descr_1 [shape=plaintext label=<
+            <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
+                  <TR>
+                        <TD PORT="descr"  BGCOLOR="darkgrey">longlonglonglonglong description<BR/>description<BR/>description</TD>
+                        <TD PORT="return" BGCOLOR="orange">return</TD>
+                  </TR>
+            </TABLE>>];        }
+
+        subgraph clusterF0 {
+            bgcolor=green;
+            pencolor=black;
+            label = "int smt0 (int pr01, float pr02)";
+            labeljust=c; 
+            descr_0 [shape=plaintext label=<
+            <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
+                  <TR>
+                        <TD PORT="descr"  BGCOLOR="darkgrey">longlonglonglonglong description<BR/>description<BR/>description</TD>
+                        <TD PORT="return" BGCOLOR="orange">return</TD>
+                  </TR>
+            </TABLE>>];
+        }
     }
         
     subgraph cluster2 {
@@ -89,7 +89,7 @@ digraph sch21 {
         label = "OUTPUT";  
         labeljust=c;              
         node [style=filled fillcolor=grey];
-        c [label="Результат"]  
+        c [label="Результат"]   
     }
     
     0 [label="INPUT" style=filled fillcolor=grey];
@@ -105,11 +105,14 @@ digraph sch21 {
 
 sch21_maze = """
 digraph sch21 {
-    rankdir=LR;
-    graph [newrank = true]; 
+    rankdir=LR;    
+    # graph [nodesep=0.05 newrank = true splines=spline]; 
+    graph [nodesep=0.05 ranksep=2 newrank = true concentrate=true]; 
     node [shape=box];
-          
-    subgraph cluster0 {   
+    label="Black Box solving ???";
+              
+    subgraph clusterINPUT {   
+        rank=same;
         bgcolor=grey;
         node [style=filled fillcolor=red];
         label = "INPUT";
@@ -163,7 +166,11 @@ digraph sch21 {
                         <TD PORT="r1"  BGCOLOR="darkgrey">The user sets the starting and ending points <BR/></TD>
                   </TR>
                   <TR>
-                        <TD PORT="r2"  BGCOLOR="darkgrey">The route, which is the solution, must be displayed <BR/>with "a line" 1 character thick, passing through the middle of all the <BR/>cells in the maze through <BR/>which the solution runs</TD>
+                        <TD PORT="r2"  BGCOLOR="darkgrey">
+The route, which is the solution, must be displayed <BR/>
+    with "a line" 1 character thick, passing through the middle of all the <BR/>
+    cells in the maze through which the solution runs
+                        </TD>
                   </TR>
                   <TR>
                         <TD PORT="r3"  BGCOLOR="darkgrey">The color of the solution line must be different <BR/>from the color of the walls, and the field</TD>
@@ -234,7 +241,7 @@ digraph sch21 {
             
     }
         
-    subgraph cluster1 {
+    subgraph clusterMAGIC {
         bgcolor=orange;
         node [style=filled fillcolor=green]; 
         rank=same
@@ -242,38 +249,157 @@ digraph sch21 {
         labeljust=c;
         
         b0 [label="Что тут может происходить"];
+                
+        subgraph clusterPart3_sol {
+            bgcolor=green;
+            pencolor=black;
+            label = "Part 3";
+            labeljust=c;
+            solPart3 [shape=plaintext label=<
+            <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4"> 
+                <TR> 
+                    <TD PORT="findSolution" BGCOLOR="darkgrey" BALIGN="LEFT" ROWSPAN="3">struct *maze findSolution(const struct *maze)</TD>
+                </TR>
+                <TR> 
+                    <TD PORT="findSolution1" BGCOLOR="darkgrey" BALIGN="LEFT" HREF="https://habr.com/ru/articles/578110/">Aлгоритм отрисовки линий Брезенхэма</TD>
+                </TR>  
+                <TR> 
+                    <TD PORT="findSolution2" BGCOLOR="darkgrey" BALIGN="LEFT" HREF="https://habr.com/ru/companies/otus/articles/748470/">Алгоритм Дейкстры и А*</TD>
+                </TR>     
+                <TR>
+                    <TD BGCOLOR="darkgrey" PORT="solutionTest" COLSPAN="2" >write tests for solution of maze</TD>
+                </TR> 
+            </TABLE>>]; 
+            # findSolution1 [label="Aлгоритм отрисовки линий Брезенхэма" URL="https://habr.com/ru/articles/578110/" fillcolor=darkgrey]
+            # findSolution2 [label="Алгоритм Дейкстры и А*" URL="https://habr.com/ru/companies/otus/articles/748470/" fillcolor=darkgrey]
+        } 
         
-        # subgraph clusterF1 {
-        #     bgcolor=green;
-        #     pencolor=black;
-        #     label = "void smt1 (ch str, double c)";
-        #     labeljust=c;
-        #     descr_1 [shape=plaintext label=<
-        #     <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
-        #           <TR>
-        #                 <TD PORT="descr"  BGCOLOR="darkgrey">longlonglonglonglong description<BR/>description<BR/>description</TD>
-        #                 <TD PORT="return" BGCOLOR="orange">return</TD>
-        #           </TR>
-        #     </TABLE>>];        
-        # }
+        subgraph clusterPart2_sol {
+            bgcolor=green;
+            pencolor=black;
+            label = "Part 2";
+            labeljust=c;
+            solPart2 [shape=plaintext label=<
+            <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
+                 <TR>
+                       <TD BGCOLOR="darkgrey" PORT="info" HREF="https://habr.com/ru/articles/176671/">habr info</TD>
+                       <TD BGCOLOR="darkgrey" HREF="https://tproger.ru/articles/maze-generators">tproger info</TD>
+                       <TD BGCOLOR="darkgrey" HREF="https://www.educative.io/courses/mazes-for-programmers/understanding-ellers-algorithm">educative info</TD>
+                 </TR>
+                 <TR>
+                       <TD BGCOLOR="darkgrey" PORT="checkMaze" COLSPAN="3" BALIGN="LEFT">int checkMaze(const struct *maze)<BR/>     
+    # тут использовать маску для "loop" <BR/>     
+    # для "isolated areas" мб найти клетки с тремя  <BR/>     
+    # границами и как-то найти, что область замкнута                  
+                       </TD>
+                 </TR>
+                 <TR>
+                       <TD BGCOLOR="darkgrey" PORT="genTest" COLSPAN="3">write tests for generationg of maze</TD>
+                 </TR>                 
+                 <TR>
+                       <TD PORT="mazeSave"  BGCOLOR="darkgrey" BALIGN="LEFT" COLSPAN="3">int mazeSave(ch* path/to/file, const struct *maze)</TD>
+                 </TR>
+            </TABLE>>];        
+        }        
         
+        subgraph clusterPart1_sol {
+            bgcolor=green;
+            pencolor=black;
+            label = "Part 1";
+            labeljust=c;
+            solPart1 [shape=plaintext label=<
+            <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
+                  <TR>
+                        <TD PORT="struct"  BGCOLOR="darkgrey" BALIGN="LEFT">struct maze {int n_M, m_M, n, m; <BR/>int** hBorders; int** vBorders;}</TD>
+                  </TR>
+                  <TR>
+                        <TD PORT="gui"  BGCOLOR="darkgrey">int main(int argv, ch* argc)</TD>
+                  </TR>
+                  <TR>
+                        <TD PORT="initMaze"  BGCOLOR="darkgrey" BALIGN="LEFT">int initMaze(ch* path/to/file, n, m):<BR/>
+    int rez=0;<BR/>
+    if (!path/to/file) generateMaze(n, m);<BR/>
+    else (loadFile(path/to/file) = rez);<BR/>
+    return rez;
+                        </TD>
+                  </TR>
+                  <TR>
+                        <TD PORT="loadFile"  BGCOLOR="darkgrey" BALIGN="LEFT">
+int loadFile(ch* path/to/file)<BR/>
+        # load in 'r' mode</TD>
+                  </TR>
+                  <TR>
+                        <TD PORT="generateMaze"  BGCOLOR="darkgrey" BALIGN="LEFT">
+struct *maze generateMaze(int n, int m)<BR/>
+        # load in 'r' mode</TD>
+                  </TR>
+                  <TR>
+                        <TD PORT="checkSize"  BGCOLOR="darkgrey" BALIGN="LEFT">int checkSize(const struct *maze):<BR/>
+    if (*maze.n &gt; *maze.n_M || <BR/>         *maze.m &gt; *maze.m_M):<BR/>     
+    return ERROR;
+                        </TD>
+                  </TR>
+                  <TR>
+                        <TD PORT="renderMaze"  BGCOLOR="darkgrey" HREF="https://clubmate.fi/using-pseudographics-in-blogposts-drawing-ascii-diagrams-and-boxes" BALIGN="LEFT">
+int render4090RTX(const struct *maze,<BR/>
+    const float* pathC[3],<BR/>
+    const float* edgeC[3])<BR/>
+    const float* fieldC[3])<BR/>
+    #МБ [|-\] - набор символов
+                        </TD>
+                  </TR>
+                  <TR>
+                        <TD PORT="wtf"  BGCOLOR="darkgrey">не понял условие</TD>
+                  </TR>
+            </TABLE>>];        
+        }
     }
         
-    subgraph cluster2 {
+    subgraph clusterOUTPUT {
         bgcolor=red;     
         label = "OUTPUT";  
         labeljust=c;              
         node [style=filled fillcolor=grey];
         c0 [label="Результат"]  
+        c1 [label="Проект на 100%"] 
     }
     
     0 [label="INPUT" style=filled fillcolor=grey];
     1 [label="MAGIC" style=filled fillcolor=orange];
     2 [label="OUTPUT" style=filled fillcolor=red];
 
-    a0 -> b0 -> c0;
-    0 -> 1 -> 2;    
-    # {rank=same {descr_1:r1 -> descr_2:r1 -> descr_3:r1 [style=invis]}}
+    0 -> 1 -> 2;
+    {rank=same {2 -> c1 -> c0 [style=invis]}}         
+    
+    solPart1:initMaze     :e -> solPart1:gui:e      [arrowhead=diamond penwidth=2];
+    solPart1:checkSize    :e -> solPart1:gui:e      [arrowhead=diamond penwidth=2];
+    solPart1:wtf          :e -> solPart1:gui:e      [arrowhead=diamond penwidth=2]; 
+    solPart1:loadFile     :e -> solPart1:initMaze:e [arrowhead=diamond penwidth=2 color="blue"]; 
+    solPart1:generateMaze :e -> solPart1:initMaze:e [arrowhead=diamond penwidth=2 color="blue"]; 
+    solPart2:checkMaze    :e -> solPart1:gui:e      [arrowhead=diamond penwidth=2];
+    solPart2:mazeSave     :e -> solPart1:gui:e      [arrowhead=diamond penwidth=2];
+    solPart1:renderMaze   :e -> solPart1:gui:e      [arrowhead=diamond penwidth=2];    
+    solPart3:findSolution1:se -> solPart1:gui:e      [arrowhead=diamond penwidth=2];
+    
+    descr_3:r1            -> solPart1:gui        [arrowhead=diamond penwidth=2]; 
+    descr_3:r2            -> solPart1:renderMaze [arrowhead=diamond penwidth=2]; 
+    descr_3:r3            -> solPart1:renderMaze [arrowhead=diamond penwidth=2]; 
+    descr_3:r4            -> solPart3:solutionTest [arrowhead=diamond penwidth=2]; 
+     
+    descr_1:r1            -> solPart1:gui        [arrowhead=diamond penwidth=2]; 
+    descr_2:r5            -> solPart1:initMaze        [arrowhead=diamond penwidth=2];  
+    descr_2:r6            -> solPart2:mazeSave   [arrowhead=diamond penwidth=2];  
+    
+    descr_2:r2:e -> solPart2:info:w        [arrowhead=diamond penwidth=2];     
+    descr_2:r3:w -> a:flaws:w             [arrowhead=diamond penwidth=2];
+    descr_2:r7:w -> descr_1:r4:w          [arrowhead=diamond penwidth=2];
+    descr_2:r3:e -> solPart2:checkMaze:w  [arrowhead=diamond penwidth=2];
+    descr_2:r4:e -> solPart2:genTest:w    [arrowhead=diamond penwidth=2];
+    
+    descr_1:r2 -> solPart1:loadFile  [arrowhead=diamond penwidth=2];
+    descr_1:r3 -> solPart1:checkSize [arrowhead=diamond penwidth=2];
+    descr_1:r4 -> solPart1:renderMaze    [arrowhead=diamond penwidth=2];
+    descr_1:r5 -> solPart1:wtf       [arrowhead=diamond penwidth=2];
 }
 
 """
@@ -282,4 +408,4 @@ digraph sch21 {
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    graph_viz_save(sch21_maze, "sch21_maze", 0)
+    graph_viz_save(sch21_maze, "sch21_maze", 1)
